5230b94bf39dddf47ce932f77509b4c97f420244
G-Carneiro <carneiro.gabr@gmail.com>
2022-03-17 20:07:38 -0300

    earliest deadline first

--- sched/RealTime.py
+++ sched/RealTime.py
@@ -13,6 +13,8 @@ class Task:
         self._remaining_computation_time: float = computation_time
         self.period: float = period
         self.deadline: float = period
+        self.absolute_deadline: float = period
+        self.next_occurrence: float = 0
         self.deadline_misses: List[float] = []
         self.preemptions: List[float] = []
 
@@ -43,62 +45,87 @@ class PeriodicTasks:
         # [(start, end), ...]
         self._scheduling_periods: Dict[Task, List[Tuple[float, float]]] = {task: [] for task in tasks}
         self._idle_periods: List[Tuple[float, float]] = []
+        self._time_line = []
 
-    def rate_monotonic(self, final_time: float) -> list:
+    def rate_monotonic(self, final_time: float) -> None:
         priority_tasks: List[Task] = sorted(self._tasks, key=lambda x: x.period)
-        next_occurrences: Dict[Task, float] = {task: 0 for task in self._tasks}
-        time_line = []
 
         while (self._actual_time < final_time):
-            for i in range(len(priority_tasks)):
-                task: Task = priority_tasks[i]
-                if (next_occurrences[task] <= self._actual_time):
-                    actual_task = task
-                    actual_task_id: int = i
-                    break
-            else:
-                time_line += [self._actual_time, None]
-                next_occurrence = min(next_occurrences.values())
-                self._idle_periods.append((self._actual_time, next_occurrence))
-                self._actual_time = next_occurrence
-                continue
+            self._scheduling(priority_tasks)
 
-            execution_time: float = actual_task.get_remaining_computation_time()
-            new_time: float = self._actual_time + execution_time
-            time_line += [self._actual_time, actual_task]
+        return None
 
-            for i in range(actual_task_id):
-                next_occurrence = next_occurrences[priority_tasks[i]]
-                if (new_time > next_occurrence):
-                    execution_time = next_occurrence - self._actual_time
-                    new_time = next_occurrence
+    def earliest_deadline_first(self, final_time: float) -> None:
+        while (self._actual_time < final_time):
+            priority_tasks: List[Task] = sorted(self._tasks, key=lambda x: x.absolute_deadline)
+            self._scheduling(priority_tasks)
 
-            for i in range(actual_task_id + 1, len(priority_tasks)):
-                task = priority_tasks[i]
-                next_occurrence = next_occurrences[task]
-                if (new_time >= next_occurrence + task.deadline):
-                    next_occurrences[task] += task.deadline
-                    task.deadline_misses.append(next_occurrences[task])
-                    task.att_remaining_computation_time(task.get_remaining_computation_time())
+        return None
 
-            actual_task.att_remaining_computation_time(execution_time)
-            if (actual_task.get_remaining_computation_time() == actual_task.get_computation_time()):
-                next_occurrences[actual_task] = min(self._actual_time, next_occurrences[actual_task]) \
-                                                + actual_task.deadline
-            else:
-                actual_task.preemptions.append(new_time)
+    def _scheduling(self, priority_tasks: List[Task]) -> None:
+        for i in range(len(priority_tasks)):
+            task: Task = priority_tasks[i]
+            if (task.next_occurrence <= self._actual_time):
+                actual_task = task
+                actual_task_id: int = i
+                break
+        else:
+            self._time_line += [self._actual_time, None]
+            next_occurrence = min([task.next_occurrence for task in self._tasks])
+            self._idle_periods.append((self._actual_time, next_occurrence))
+            self._actual_time = next_occurrence
+            return None
 
-            self._scheduling_periods[actual_task].append((self._actual_time, new_time))
-            self._actual_time = new_time
+        execution_time: float = actual_task.get_remaining_computation_time()
+        new_time: float = self._actual_time + execution_time
+        self._time_line += [self._actual_time, actual_task]
 
-        return time_line
+        for i in range(actual_task_id):
+            next_occurrence = priority_tasks[i].next_occurrence
+            if (new_time > next_occurrence):
+                execution_time = next_occurrence - self._actual_time
+                new_time = next_occurrence
 
-    def earliest_deadline_first(self) -> None:
-        pass
+        for i in range(actual_task_id + 1, len(priority_tasks)):
+            task = priority_tasks[i]
+            next_occurrence = task.next_occurrence
+            if (new_time >= next_occurrence + task.deadline):
+                task.next_occurrence += task.deadline
+                task.deadline_misses.append(task.next_occurrence)
+                task.att_remaining_computation_time(task.get_remaining_computation_time())
+
+        actual_task.att_remaining_computation_time(execution_time)
+        if (actual_task.get_remaining_computation_time() == actual_task.get_computation_time()):
+            actual_task.next_occurrence = min(self._actual_time, actual_task.next_occurrence) \
+                                          + actual_task.deadline
+            actual_task.absolute_deadline += actual_task.deadline
+        else:
+            actual_task.preemptions.append(new_time)
+
+        self._scheduling_periods[actual_task].append((self._actual_time, new_time))
+        self._actual_time = new_time
+
+        return None
+
+    def show_results(self) -> None:
+        deadline_misses: int = 0
+        for task in self._tasks:
+            deadline_misses += len(task.deadline_misses)
+            print(f"A tarefa {task.name} é \n"
+                  f"- Escalonada {len(self._scheduling_periods[task])} vezes em {self._scheduling_periods[task]} \n"
+                  f"- Preemptada {len(task.preemptions)} vezes em {task.preemptions} \n"
+                  f"- Tem {len(task.deadline_misses)} perdas de deadline em {task.deadline_misses}\n")
+
+        print(f"Total de perdas de deadline: {deadline_misses} \n"
+              f"Timeline: \n"
+              f"{self._time_line}")
+
+        return None
 
 
-a = Task("A", computation_time=20, period=100)
-b = Task("B", computation_time=40, period=150)
+a = Task("A", computation_time=5, period=10)
+b = Task("B", computation_time=2, period=4)
 c = Task("C", computation_time=100, period=350)
-sched = PeriodicTasks([a, b, c])
-print(sched.rate_monotonic(360))
+sched = PeriodicTasks([a, b])
+sched.earliest_deadline_first(30)
+sched.show_results()
